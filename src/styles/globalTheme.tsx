152a5ccfa91ab3b6b0707e829328cef146bbb712
overist <wj@over.ist>
2025-01-17 16:07:53 +0900

    style: 리소스폴더 셋업, 리셋CSS/디폴트CSS/MUI디자인시스템 정의

---
+++ src/styles/globalTheme.tsx
@@ -0,0 +1,134 @@
+import { createTheme } from '@mui/material'
+
+export const globalTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2' // 기본 색상
+    },
+    secondary: {
+      main: '#dc004e' // 보조 색상
+    },
+    background: {
+      default: '#f5f5f5' // 배경색
+    }
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+    button: {
+      textTransform: 'none' // 버튼의 텍스트 소문자 유지
+    },
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    h2: {
+      fontSize: '2.5rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    h3: {
+      fontSize: '2rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    h4: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    h5: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    h6: {
+      fontSize: '1rem',
+      fontWeight: 500,
+      lineHeight: 1.2
+    },
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.5
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.5
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      fontWeight: 500,
+      lineHeight: 1.5
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      lineHeight: 1.5
+    }
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          height: 'fit-content',
+          borderRadius: '0.3rem',
+          '& .MuiChip-label': {
+            padding: '0.3rem'
+          }
+        }
+      }
+    },
+
+    MuiButton: {
+      // need to remove hover effect
+      defaultProps: {
+        variant: 'text',
+        disableRipple: true,
+        disableElevation: true,
+        disableFocusRipple: true,
+        disableTouchRipple: true
+      },
+      styleOverrides: {
+        root: {
+          ':hover': {
+            backgroundColor: 'unset'
+          }
+        }
+      }
+    },
+    MuiIconButton: {
+      defaultProps: {
+        disableRipple: true,
+        disableFocusRipple: true,
+        disableTouchRipple: true
+      },
+      styleOverrides: {
+        root: {
+          ':hover': {
+            backgroundColor: 'unset'
+          }
+        }
+      }
+    },
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          boxShadow: 'none' // 그림자 제거
+        }
+      }
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'none', // 추가적으로 그림자가 있을 경우 제거
+          ':hover': {
+            backgroundColor: 'unset' // 호버 시 배경 제거
+          }
+        }
+      }
+    }
+  }
+})
